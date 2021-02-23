d19ea4dfab2d3925f2b2432feb1104361ada4a07
Rajesh-0907 <rajeshraichu529@gmail.com>
2021-02-23 22:34:24 +0530

    mernstack

---
+++ mern-backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express();
+
+const port = 9000;
+
+app.get('/', (req, res) =>res.status(200).send("helloworld"))
+
+app.listen(port, () => console.log(`listening on ${port}`))
